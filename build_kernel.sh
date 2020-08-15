e7294d9521d27f52246719d375d74e5c676a5966
Jesse Chan <jc@lineageos.org>
2020-08-15 04:17:49 +0300

    ARM64: configs: vendor: lightning_r9q_defconfig: enable DT building in defconfig
    Signed-off-by: Jesse Chan <jc@linux.com>
    Signed-off-by: Shreyash Wasnik <shreyashwasnik112@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -7,7 +7,7 @@ mkdir out
 BUILD_CROSS_COMPILE=$(pwd)/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 KERNEL_LLVM_BIN=$(pwd)/toolchain/llvm-arm-toolchain-ship/10.0/bin/clang
 CLANG_TRIPLE=aarch64-linux-gnu-
-KERNEL_MAKE_ENV="CONFIG_BUILD_ARM64_DT_OVERLAY=y"
+KERNEL_MAKE_ENV=""
 
 make -j64 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE CONFIG_SECTION_MISMATCH_WARN_ONLY=y vendor/r9q_eur_openx_defconfig
 make -j64 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE CONFIG_SECTION_MISMATCH_WARN_ONLY=y
